69a302ab35905628e680d8438c66ab7c54a81a1a
Ture Hassler <ture.hassler@gmail.com>
2023-05-09 15:46:51 +0200

    Started project

---
+++ Project/project.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+/*
+Project in Parallel & Distributed Computing
+Uppsala University, VT23
+
+Monte Carlo computations & Stochastic Simulation algorithm to simulate malaria epidemic
+
+By Ture Hassler
+*/
+
+
+
+
+
+
